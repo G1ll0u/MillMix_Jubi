bd92fb60df82e3654384637058a462dea241686d
Your Name <you@example.com>
2025-07-06 04:00:21 +0200

    Fix mod loading :D

--- src/main/java/com/jubitus/millmix/MillMix.java
+++ src/main/java/com/jubitus/millmix/MillMix.java
@@ -1,10 +1,16 @@
 package com.jubitus.millmix;
 
+import com.jubitus.millmix.command.CommandGetReputation;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import org.spongepowered.asm.launch.MixinBootstrap;
 import org.spongepowered.asm.mixin.Mixins;
 
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, dependencies = "required-after:millenaire")
 public class MillMix {
+    @Mod.EventHandler
+    public void serverLoad(FMLServerStartingEvent event) {
+        event.registerServerCommand(new CommandGetReputation());
+    }
 }
